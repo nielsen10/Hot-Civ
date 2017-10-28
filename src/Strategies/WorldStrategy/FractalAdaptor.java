7d85f44b0a0f9e951bbc74828931a3e46e094cbf
Eichwald <you@example.com>
2017-10-28 15:55:43 +0200

    assignment7new

--- src/Strategies/WorldStrategy/FractalAdaptor.java
+++ src/Strategies/WorldStrategy/FractalAdaptor.java
@@ -1,7 +1,62 @@
 package Strategies.WorldStrategy;
 
+import hotciv.framework.GameConstants;
+import hotciv.framework.Position;
+import hotciv.standard.CityImpl;
+import hotciv.standard.GameImpl;
+import hotciv.standard.TileImpl;
+import hotciv.standard.UnitImpl;
+import thirdparty.ThirdPartyFractalGenerator;
+
+import java.util.HashMap;
+import java.util.concurrent.SynchronousQueue;
+
 /**
  * Created by csdev on 10/28/17.
  */
-public class FractalAdaptor {
+public class FractalAdaptor implements WorldStrategy {
+    private ThirdPartyFractalGenerator generator;
+
+    public FractalAdaptor() {
+        generator = new ThirdPartyFractalGenerator();
+    }
+
+    @Override
+    public void buildWorld(GameImpl game, HashMap<Position, UnitImpl> unitMap, HashMap<Position, TileImpl> tileMap, HashMap<Position, CityImpl> cityMap) {
+        buildFractal(tileMap);
+    }
+
+    private void buildFractal(HashMap<Position,TileImpl> tileMap) {
+        for(int i=0; i<=15; i++) {
+            for(int j=0; j<=15; j++) {
+                Position pos = new Position(i,j);
+
+                char tileChar = generator.getLandscapeAt(i, j);
+                String type = "error";
+                if (tileChar == '.') {
+                    type = GameConstants.OCEANS;
+                }
+                if (tileChar == 'o') {
+                    type = GameConstants.PLAINS;
+                }
+                if (tileChar == 'M') {
+                    type = GameConstants.MOUNTAINS;
+                }
+                if (tileChar == 'f') {
+                    type = GameConstants.FOREST;
+                }
+                if (tileChar == 'h') {
+                    type = GameConstants.HILLS;
+                }
+
+                tileMap.put(pos, new TileImpl(pos, type));
+                System.out.print(tileChar);
+            }
+            System.out.println("");
+        }
+    }
+
+    public static void main(String[] args) {
+        new FractalAdaptor().buildFractal(new HashMap<>());
+    }
 }
